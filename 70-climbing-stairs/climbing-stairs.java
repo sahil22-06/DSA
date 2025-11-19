7de7af7c7c872c32edc3354a8e47bf73fe4c41d4
Sahil Shaikh <sahilsss2004@gmail.com>
2025-11-19 23:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (7.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
-
+//sahil
         int prev1 = 2;  // ways to reach step (n-1)
         int prev2 = 1;  // ways to reach step (n-2)
         
